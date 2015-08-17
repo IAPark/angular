98ed6d9b3c3c81bd67b0d06514db4d7c4b7ff37e
Isaac Park <isaac.a.park@gmail.com>
2015-08-16 20:32:51 -0400

    fix(docs): Replaces old syntax for hostListeners
    Updates an outdated example of hostListeners in the DirectiveAnotation to work in alpha34

--- modules/angular2/src/core/annotations_impl/annotations.ts
+++ modules/angular2/src/core/annotations_impl/annotations.ts
@@ -246,9 +246,9 @@ import {DEFAULT} from 'angular2/change_detection';
  *   properties: [
  *     'text: tooltip'
  *   ],
- *   hostListeners: {
- *     'onmouseenter': 'onMouseEnter()',
- *     'onmouseleave': 'onMouseLeave()'
+ *   host: {
+ *     '(mouseenter)': 'onMouseEnter()',
+ *     '(onmouseleave)': 'onMouseLeave()'
  *   }
  * })
  * class Tooltip{
